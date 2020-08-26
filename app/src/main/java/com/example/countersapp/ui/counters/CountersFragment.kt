440efa2401a5cf7cfee7e93420aa10f5f26507fe
José Luis Beltrán <jlbeltran94@gmail.com>
2020-08-26 01:56:16 -0500

    *code format

--- app/src/main/java/com/example/countersapp/ui/counters/CountersFragment.kt
+++ app/src/main/java/com/example/countersapp/ui/counters/CountersFragment.kt
@@ -3,11 +3,11 @@ package com.example.countersapp.ui.counters
 import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.viewbinding.ViewBinding
 import com.example.countersapp.databinding.FragmentCountersBinding
