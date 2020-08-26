fa3df2e65d2badcee1339eef5aa02f4920831c46
José Luis Beltrán <jlbeltran94@gmail.com>
2020-08-26 02:11:29 -0500

    *code format

--- app/src/main/java/com/example/countersapp/ui/createcounters/CreateCounterFragment.kt
+++ app/src/main/java/com/example/countersapp/ui/createcounters/CreateCounterFragment.kt
@@ -2,15 +2,13 @@ package com.example.countersapp.ui.createcounters
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.countersapp.databinding.FragmentCreateCounterBinding
 import com.example.countersapp.util.invisible
-import com.example.countersapp.util.visible
 import dagger.hilt.android.AndroidEntryPoint
 
 
