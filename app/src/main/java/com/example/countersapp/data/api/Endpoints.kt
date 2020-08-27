4897095519247206211306f7ffba6d9bf647052e
José Luis Beltrán <jlbeltran94@gmail.com>
2020-08-27 00:29:39 -0500

    URL_BASE was updated to match localhost when using emulator

--- app/src/main/java/com/example/countersapp/data/api/Endpoints.kt
+++ app/src/main/java/com/example/countersapp/data/api/Endpoints.kt
@@ -1,7 +1,7 @@
 package com.example.countersapp.data.api
 
 object Endpoints {
-    const val URL_BASE = "http://192.168.1.107:3000/"
+    const val URL_BASE = "http://10.0.2.2:3000/"
     const val GET_COUNTERS = "api/v1/counters"
     const val CREATE_COUNTER = "api/v1/counter"
     const val INCREASE_COUNTER = "api/v1/counter/inc"
